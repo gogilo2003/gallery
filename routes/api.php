77fc1140a0ba05180452a8a8715fdc7c9ee606d6
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2021-09-09 05:11:27 +0300

    Image Listing error fixed

--- routes/api.php
+++ routes/api.php
@@ -4,35 +4,36 @@ use Illuminate\Support\Facades\Route;
 use Ogilo\Gallery\Models\Album;
 
 Route::group(['middleware' => 'api', 'as' => 'api', 'prefix' => 'api', 'namespace' => 'Ogilo\Gallery\Http\Controllers\Api\V1'], function () {
+    Route::group(['middleware' => 'auth:api', 'as' => '-admin', 'prefix' => 'admin'], function () {
+        Route::group(['as' => '-albums', 'prefix' => 'albums'], function () {
+            Route::get('{id?}', ['as' => '', 'uses' => 'AlbumController@index']);
+            Route::post('', ['as' => '-create', 'uses' => 'AlbumController@create']);
+            Route::patch('', ['as' => '-update', 'uses' => 'AlbumController@update']);
+            Route::delete('', ['as' => '-delete', 'uses' => 'AlbumController@delete']);
+        });
 
-	Route::group(['middleware' => 'auth:api', 'as' => '-admin', 'prefix' => 'admin'], function () {
-		Route::group(['as' => '-albums', 'prefix' => 'albums'], function () {
-			Route::get('{id?}', ['as' => '', 'uses' => 'AlbumController@index']);
-			Route::post('', ['as' => '-create', 'uses' => 'AlbumController@create']);
-			Route::patch('', ['as' => '-update', 'uses' => 'AlbumController@update']);
-			Route::delete('', ['as' => '-delete', 'uses' => 'AlbumController@delete']);
-		});
+        Route::group(['as' => '-pictures', 'prefix' => 'pictures'], function () {
+            Route::get('{id?}', ['as' => '', 'uses' => 'PictureController@index']);
+            Route::post('', ['as' => '-create', 'uses' => 'PictureController@create']);
+            Route::post('update', ['as' => '-update', 'uses' => 'PictureController@update']);
+            Route::delete('', ['as' => '-delete', 'uses' => 'PictureController@delete']);
+            Route::post('publish', ['as' => '-publish', 'uses' => 'PictureController@publish']);
+        });
+    });
 
-		Route::group(['as' => '-pictures', 'prefix' => 'pictures'], function () {
-			Route::get('{id?}', ['as' => '', 'uses' => 'PictureController@index']);
-			Route::post('', ['as' => '-create', 'uses' => 'PictureController@create']);
-			Route::post('update', ['as' => '-update', 'uses' => 'PictureController@update']);
-			Route::delete('', ['as' => '-delete', 'uses' => 'PictureController@delete']);
-			Route::post('publish', ['as' => '-publish', 'uses' => 'PictureController@publish']);
-		});
-	});
+    Route::prefix('gallery')
+        ->name('-gallery')
+        ->group(function () {
+            Route::get('albums', function () {
+                $albums = Album::get();
 
-	Route::prefix('gallery')
-		->name('-gallery')
-		->group(function () {
-			Route::get('albums', function () {
-				$albums = Album::get();
-				return response()->json($albums, 200);
-			})->name('-albums');
+                return response()->json($albums, 200);
+            })->name('-albums');
 
-			Route::get('pictures/{id?}', function ($id=null) {
-				$pictures = Album::where('published', 1)->paginate(4);
-				return response()->json($pictures, 200);
-			})->name('-pictures');
-		});
+            Route::get('pictures/{id?}', function ($id = null) {
+                $pictures = Album::paginate(4);
+
+                return response()->json($pictures, 200);
+            })->name('-pictures');
+        });
 });
